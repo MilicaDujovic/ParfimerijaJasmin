07f72593980bca1db27a3af3f4fd54f3d8dff9fe
MilicaDujovic <131909456+MilicaDujovic@users.noreply.github.com>
2023-05-03 14:42:47 +0200

    Update Strings.javajbkj

--- src/main/java/Strings.java
+++ src/main/java/Strings.java
@@ -5,9 +5,7 @@ public class Strings {
     public static final String MUSKI_PARFEMI_URL = "https://www.jasmin.rs/muski-parfemi.html";
     public static final String LOGIN_PAGE_URL = "https://www.jasmin.rs/customer/account/login/";
     public static final String CREATE_ACCOUNT_URL = "https://www.jasmin.rs/customer/account/create/";
-
     public static final String ACCOUNT_URL = "https://www.jasmin.rs/customer/account/";
-
     public static final String SENKE_URL = "https://www.jasmin.rs/sminka/oci/senke.html";
     public static final String CUSTOMER_URL = "https://www.jasmin.rs/jasmin/customer/";
     public static final String PRIVACY_POLICY_URL = "https://www.jasmin.rs/politika-privatnosti";
@@ -21,11 +19,8 @@ public class Strings {
     public static final String YOUR_FIRST_NAME = "MILICA";
     public static final String YOUR_LAST_NAME = "Markovic";
     public static final String YOUR_EMAIL = "markovic.milica@gmail.com";
-
     public static final String VALID_EMAIL_PART_1 = "markovic.milica";
-
     public static final String VALID_EMAIL_PART_2 = "@gmail.com";
-
     public static final String INVALID_EMAIL = "dujoooovic.milica@gmail.com";
     public static final String YOUR_PASSWORD = "Milica1*";
     public static final String INVALID_PASSWORD = "Milica";
